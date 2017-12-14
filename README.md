093ce0d6bdcb0615bc2dbca1f1f0c56432d3e869
Patrick <pbergeron92@gmail.com>
2017-12-14 12:22:22 -0500

    Include the script in the reposiroty

---
+++ README.md
@@ -0,0 +1,52 @@
+This utility copy a playlist file (m3u8) through ffmpeg
+
+The playlist is converted as a unified mkv file.
+
+# Install
+
+1. Copy the repository.
+2. With the command prompt move within the folder directory.
+3. Run `npm install`.
+
+# Usage
+
+## Environement variable
+
+`PL_DESTINATION` : The destination of the files.
+
+## Arguments
+
+* `playlist-copy.js [-d <destination>] -f <file path>`
+* `playlist-copy.js [-d <destination>] -u <playlist path> -t <title> -s <season number> -e <episode number>`
+
+`-d` | `--destination`: The destination of the files.
+
+`-f` | `--file`       : Input file path.
+
+`-u` | `--url`        : The playlist file url/path.
+
+`-t` | `--title`      : The title of the show. (ex: "Breaking Bad")
+
+`-s` | `--season`     : The season number. (ex: 02)
+
+`-e` | `--episode`    : The episode number. (ex: 06)
+
+## Input file
+
+```
+[
+	{
+		"playlist_url": "https://.../BreakingBadS01E06.m3u8",
+		"title": "Breaking Bad",
+		"season": "01",
+		"episode": "06"
+	}, ...
+]
+```
+
+# File structure
+
+The files created by the utility will be stored as followed.
+
+`<PL_DESTINATION|destination>/<title>/Season <season>/<title> S<season>E<episode>.mkv`
+
---
+++ show-playlist-copy.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+
+const process = require('process');
+const subprocess = require('child_process');
+const argv = require('minimist')(process.argv.splice(2));
+
+const ffmpeg_path = subprocess.execSync('which ffmpeg').toString().slice(0, -1);
+const base_path =  argv.d || argv.destination || process.env.PL_DESTINATION;
+const concurrent = 5;
+
+let entries = [], current = 0;
+
+(function main(argv) {
+	if (argv.help || argv.h) {
+		console.log('Usages:');
+		console.log('\t(...) -h');
+		console.log('\t(...) -f file');
+		console.log('\t(...) [--url|-u playlist_url] [--title|-t title] [--season|-s season_num] [--episode|-e episode_num]');
+	} else if (argv.file || argv.f) {
+		entries = require(argv.file || argv.f);
+		for (let i = 0; i < concurrent; i++) {
+			next();
+		}
+	} else {
+		const playlist_url = (argv.url || argv.u);
+		const title = (argv.title || argv.t);
+		const season = (argv.season || argv.s);
+		const episode = (argv.episode || argv.e);
+		
+		run(
+			0,
+			playlist_url,
+			title,
+			season,
+			episode);
+	}
+})(argv);
+
+/**
+ * Run an entry.
+ */
+function run(id, playlist_url, title, season, episode) {
+	season = pad(season);
+	episode = pad(episode);
+
+	const path = `${base_path}/${title}/Season ${season}/`
+	const filename = `${title} S${season}E${episode}.mkv`;
+	const full = `${path}${filename}`;
+
+	// Create the folder
+	subprocess.execSync(`mkdir -p "${path}"`);
+	// Start copy process.
+	const process = subprocess.spawn(ffmpeg_path, [
+		'-i', playlist_url,
+		'-c', 'copy',
+		full
+	]);
+
+	// Action on output message.
+	process.stdout.on('data', (data) => {
+		console.log(`[${id}: out] ${data}`);
+	});
+
+	// Action on error message.
+	process.stderr.on('data', (data) => {
+		console.log(`[${id}: err] ${data}`);
+	});
+	
+	// Action when process is closing.
+	process.on('close', (code) => {
+		console.log(`[${id}: ifo] ${title} S${season}E${episode} done.`);
+		setTimeout(next);
+	});
+
+	process.on('error', (e) => {
+		console.log(e);
+	});
+}
+
+/**
+ * Run next entry.
+ */
+function next() {
+	const entry = entries.pop();
+	if (entry) {
+		run(
+			++current,
+			entry.playlist_url,
+			entry.title,
+			entry.season,
+			entry.episode);
+	}
+}
+
+function pad(num) {
+	if (num.toString().length == 1) {
+		return `0${num}`;
+	}
+	return num;
+}
+
